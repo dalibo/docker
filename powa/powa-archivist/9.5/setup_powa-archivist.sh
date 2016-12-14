c461699bc3c78cf6e0a707efd70cf639f6645017
Julien Rouhaud <julien.rouhaud@free.fr>
2016-12-14 23:07:10 +0100

    Force listen on all interfaces in powa-archivist images
    The official image should take care of that, but apparently fails for at
    least 9.6 version.

--- powa/powa-archivist/9.5/setup_powa-archivist.sh
+++ powa/powa-archivist/9.5/setup_powa-archivist.sh
@@ -3,6 +3,7 @@ set -e
 
 # Configure shared_preload_libraries
 gosu postgres echo "shared_preload_libraries = 'pg_stat_statements,powa,pg_qualstats,pg_stat_kcache'" >> $PGDATA/postgresql.conf
+gosu postgres echo "listen_addresses = '*'" >> $PGDATA/postgresql.conf
 
 # restart pg
 gosu postgres pg_ctl -D "$PGDATA" -w stop -m fast
